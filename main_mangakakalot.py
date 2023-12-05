aef8f79f5c0a68af96bc32641a044daef172b0fb
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-05 15:39:05 +0700

    'edit'

---
+++ main_mangakakalot.py
@@ -0,0 +1,5 @@
+from sqlalchemy import null
+from scrapers.mangakakalot import MangakakalotCrawlerFactory
+
+if __name__ == "__main__":
+    mangakakalot = MangakakalotCrawlerFactory().create_crawler().push_to_db(type='all', slug_format=True,publish=False)
