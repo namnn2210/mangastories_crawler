83caf9e57f2fce431af7771f4a0344966a2f3ca2
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-14 16:11:59 +0700

    edit

--- crawl_manhuaus.py
+++ crawl_manhuaus.py
@@ -2,4 +2,4 @@ from scrapers.manhuaus import ManhuausCrawlerFactory
 
 if __name__ == "__main__":
     manhuaus_crawler = ManhuausCrawlerFactory().create_crawler()
-    manhuaus_crawler.push_to_db(type='all',new=False,publish=True)
+    manhuaus_crawler.push_to_db(type='all', new=False, slug_format=False, publish=True)
