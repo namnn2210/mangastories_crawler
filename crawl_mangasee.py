02c2bdae449d78df68da02d35d1025338842f1eb
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-27 16:05:54 +0700

    edit

--- crawl_mangasee.py
+++ crawl_mangasee.py
@@ -1,7 +1,7 @@
-from scrapers.mangasee import MangaseeCrawlerFactory
+from scrapers.mangasee_2 import MangaseeCrawlerFactory
 
 if __name__ == "__main__":
     mangasee_crawler = MangaseeCrawlerFactory().create_crawler()
-    mangasee_crawler.crawl()
+    mangasee_crawler.get_all_manga_urls()
     # mangasee_crawler.push_to_db(type='all', slug_format=False, publish=True)
     # mangasee_crawler.push_to_db(type='all',new=False, slug_format=False, publish=True)
--- scrapers/mangasee_2.py
+++ scrapers/mangasee_2.py
@@ -66,7 +66,7 @@ class MangaseeCrawler2(Crawler):
         super().manga_enqueue(manga_url, source_site)
 
     def extract_manga_info(self, manga_url, source_site):
-        print(manga_url,source_site)
+        print(manga_url, source_site)
 
     def extract_chapter_info(self, chapter_url, source_site, manga_original_id):
         pass
