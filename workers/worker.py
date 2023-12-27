6a447b0da622a5466747e220cab8657a3a514bc5
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-27 16:42:35 +0700

    edit

--- workers/worker.py
+++ workers/worker.py
@@ -1,11 +1,9 @@
 import logging
 
-from scrapers.mangasee_2 import MangaseeCrawlerFactory
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 
 def process_manga(manga_url, source_site):
     if source_site == 'mangasee':
-        mangasee_crawler = MangaseeCrawlerFactory.create_crawler()
-        mangasee_crawler.extract_manga_info(manga_url,source_site)
+       print(manga_url)
