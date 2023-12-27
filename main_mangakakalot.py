a55133d5b0e2e39f3a17e0360070089db8c2af62
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-27 09:07:15 +0700

    edit

--- main_mangakakalot.py
+++ main_mangakakalot.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     db = Connection().mysql_connect(db_name='mangamonster_com')
     list_update_original_ids = [item[0] for item in db.query(Manga.slug_original).filter(Manga.original.like('%mangakakalot%'),Manga.status == 1).all()]
     mangakakalot = MangakakalotCrawlerFactory().create_crawler()
-
+    # print(len(list_update_original_ids))
     # list_update_original_ids = ['manga-kt987976']
     list_part = list(divide_chunks(list_update_original_ids, 10))
     for part in list_part:
