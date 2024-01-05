73608cd1c0a1874e914c155b9607304bbe9a6b9c
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-01-05 10:16:44 +0700

    edit

--- crawl_mangasee.py
+++ crawl_mangasee.py
@@ -1,7 +1,8 @@
-from scrapers.mangasee_2 import MangaseeCrawlerFactory
+# from scrapers.mangasee_2 import MangaseeCrawlerFactory
+from scrapers.mangasee import MangaseeCrawlerFactory
 
 if __name__ == "__main__":
     mangasee_crawler = MangaseeCrawlerFactory().create_crawler()
-    mangasee_crawler.get_all_manga_urls()
-    # mangasee_crawler.push_to_db(type='all', slug_format=False, publish=True)
-    # mangasee_crawler.push_to_db(type='all',new=False, slug_format=False, publish=True)
+    mangasee_crawler.crawl()
+    # mangasee_crawler.push_to_db(type='chapter', slug_format=False, publish=True)
+    mangasee_crawler.push_to_db(type='chapter', new=False, slug_format=False, publish=True)
