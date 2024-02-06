3ea87606679c4745f20d04579e356f92eb6ecc0c
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-02-06 10:56:21 +0700

    edit

--- crawl_mangasee.py
+++ crawl_mangasee.py
@@ -3,6 +3,6 @@ from scrapers.mangasee import MangaseeCrawlerFactory
 
 if __name__ == "__main__":
     mangasee_crawler = MangaseeCrawlerFactory().create_crawler()
-    # mangasee_crawler.crawl()
+    mangasee_crawler.crawl()
     # mangasee_crawler.push_to_db(type='chapter', slug_format=False, publish=True)
     mangasee_crawler.push_to_db(type='all', new=False, slug_format=False, publish=True)
